2a2958355bd7de3f537e0cf22f956f21b0f17324
Lubos Jones <l.furinda@gmail.com>
2020-01-31 15:09:25 +0100

    Updates

---
+++ precache-manifest.97bb98047b7b1a393b961e6903bb1332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e69636bf1bde0b695fac08aaab54f852",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "fc08676246d30389dbdc",
+    "url": "/react-calculator/static/css/main.08dd60b5.chunk.css"
+  },
+  {
+    "revision": "3d74d189c908c36a6ec2",
+    "url": "/react-calculator/static/js/2.56442631.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-calculator/static/js/2.56442631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc08676246d30389dbdc",
+    "url": "/react-calculator/static/js/main.adea2dd4.chunk.js"
+  },
+  {
+    "revision": "909d0137fa7114c15e08",
+    "url": "/react-calculator/static/js/runtime-main.a14a8bab.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-calculator/static/media/Pixel-LCD-7.e2897552.woff"
+  }
+]);
